8f1e596d7985c7a29336d1e341a761ba5e6a3134
bonfacethaa <thaabonface@gmail.com>
2021-08-26 08:29:29 +0300

    feat(module, refactor): add create and insert review funcs
    - change review nested for loop into function

--- sent_classifier/data_scraping/trip_advisor_scraper.py
+++ sent_classifier/data_scraping/trip_advisor_scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as soup
 from time import sleep
 
 # from database import insert_hotel
-from sent_classifier.database.database import insert_hotel
+from sent_classifier.database.database import insert_hotel, get_hotels, insert_review
 
 # configurations
 BASE_URL = 'https://www.tripadvisor.com'
@@ -14,7 +14,11 @@ scraper_headers = {
 }
 
 
-def scrap_tripadvisor(url: str):
+def scrap_hotels(url: str):
+    """
+    Function scraping hotels names, reviews and links from Trip Advisor
+    :param url:
+    """
     url = urljoin(BASE_URL, url)
 
     # 1. Fetching data from TripAdvisor
@@ -32,27 +36,52 @@ def scrap_tripadvisor(url: str):
         insert_hotel(hotel_name, reviews_num, hotel_link)
 
 
-# # loop through test and generate reviews pagination links
-# for k, v in hotel_reviews.items():
-#     hotel_link = v[0]
-#     hotel_reviews_count = v[1]
-#     hotel_reviews_count = hotel_reviews_count.split(' ')[0]
-#     hotel_reviews_count = int(hotel_reviews_count.replace(',',''))
-#     reviews_pages = []
-#     reviews_total_pages = hotel_reviews_count//5
-#     i = 0
-#     while i < reviews_total_pages:
-#         i += 5
-#         reviews_pages.append(i)
-#     tmp_hotel_link = hotel_link.split('Reviews')
-#     for i in reviews_pages:
-#         review_page_link = tmp_hotel_link[0] + 'Reviews-or' + str(i) + tmp_hotel_link[1] + '#REVIEWS'
-#         review_html = requests.get(review_page_link, headers=scraper_headers)
-#         sleep(5)
-#         hotel_reviews_soup = soup(review_html.content, features="html.parser")
-#         for review in hotel_reviews_soup.findAll('div', {'class': '_2wrUUKlw _3hFEdNs8'}):
-#             review_title = review.find('a', 'ocfR3SKN').string
-#             review = review.find('q').string
-#             review_date = review.find('span', '_34Xs-BQm').text[14:]
+# loop through test and generate reviews pagination links
+def scrap_reviews():
+    """
+    Function to fetch each hotel and scrap its reviews
+    """
+    # 1. Fetch data from the database
+    hotels = get_hotels()
+    for hotel in hotels:
+        hotel_link = hotel[3]
 
-scrap_tripadvisor('/Hotels-g294206-Kenya-Hotels.html')
+        # 2. Extract the reviews count
+        hotel_reviews_count = hotel[2]
+        hotel_reviews_count = hotel_reviews_count.split(' ')[0]
+        hotel_reviews_count = int(hotel_reviews_count.replace(',',''))
+
+        # 3. Generate reviews pages list
+        reviews_pages = []
+        reviews_total_pages = hotel_reviews_count//5
+        n = 0
+        i = 1
+        while i < reviews_total_pages + 1:
+            n += 5
+            reviews_pages.append(n)
+            i += 1
+
+        # 4. Extract part one of review url
+        tmp_hotel_link = hotel_link.split('Reviews')
+
+        # 5. loop through the review pages
+        for i in reviews_pages:
+            print("Pagination number: ",i)
+            review_page_link = tmp_hotel_link[0] + 'Reviews-or' + str(i) + tmp_hotel_link[1] + '#REVIEWS'
+            sleep(5)
+            review_html = requests.get(review_page_link, headers=scraper_headers)
+            hotel_reviews_soup = soup(review_html.content, features="html.parser")
+
+            # 6. Loop through reviews in a single review page
+            for review in hotel_reviews_soup.findAll('div', {'class': '_2wrUUKlw _3hFEdNs8'}):
+                review_title = review.find('a', 'ocfR3SKN').string
+                review_date = review.find('span', '_34Xs-BQm').text[14:]
+                review = review.find('q').string
+                print(review_title, hotel[1], review_date, review)
+
+                # 7. Insert review details to the DB
+                insert_review(review_title, hotel[1], review, review_date)
+
+
+# scrap_tripadvisor('/Hotels-g294206-Kenya-Hotels.html')
+scrap_reviews()
--- sent_classifier/database/database.py
+++ sent_classifier/database/database.py
@@ -16,7 +16,7 @@ def close_connection(connection):
         connection.close()
 
 
-def create_table():
+def create_hotel_table():
     try:
         connection = create_connection()
         cursor = connection.cursor()
@@ -29,7 +29,7 @@ def create_table():
         '''
         cursor.execute(create_table_query)
         connection.commit()
-        print("Table created successlly")
+        print("Hotels table created successlly")
         close_connection(connection)
     except (Exception, Error) as error:
         print("Error: ", error)
@@ -43,13 +43,63 @@ def insert_hotel(name, link, reviews):
         '''
         cursor.execute(insert_query, (name, link, reviews))
         connection.commit()
-        print("Item inserted successfully")
+        print("Hotel inserted successfully")
         close_connection(connection)
     except (Exception, Error) as error:
         print("Error: ", error)
 
-get
+
+def get_hotels():
+    global records
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+        get_query = '''SELECT id, name, reviews, link FROM hotel
+        '''
+        cursor.execute(get_query)
+        records = cursor.fetchall()
+        close_connection(connection)
+        return records
+    except (Exception, Error) as error:
+        print("Error: ", error)
+
+
+def create_review_table():
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+        create_table_query = '''CREATE TABLE IF NOT EXISTS review
+        (ID SERIAL PRIMARY KEY NOT NULL,
+        NAME TEXT NOT NULL,   
+        HOTEL TEXT NOT NULL,
+        REVIEW TEXT NOT NULL,
+        DATE TEXT NOT NULL,
+        UNIQUE (REVIEW));
+        '''
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Review Table created successfully")
+        close_connection(connection)
+    except (Exception, Error) as error:
+        print("Error: ", error)
+
+
+def insert_review(title, hotel, review, date):
+    try:
+        create_review_table()
+        connection = create_connection()
+        cursor = connection.cursor()
+        insert_query = '''INSERT INTO  review (NAME, HOTEL, REVIEW, DATE) VALUES (%s, %s, %s, %s)
+        '''
+        cursor.execute(insert_query, (title, hotel, review, date))
+        connection.commit()
+        print("Review inserted successfully")
+        close_connection(connection)
+    except (Exception, Error) as error:
+        print("Error: ", error)
 
 
 if __name__ == '__main__':
     insert_hotel()
+    get_hotels()
+    insert_review()
